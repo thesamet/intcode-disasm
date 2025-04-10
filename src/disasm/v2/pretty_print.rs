8cf2b15c568f05290e4a7f5e407338bd9ddbdf4e
Nadav Samet <thesamet@gmail.com>
2025-04-10 00:33:01 -0700

    Add colors

--- src/disasm/v2/pretty_print.rs
+++ src/disasm/v2/pretty_print.rs
@@ -1,91 +1,173 @@
 use crate::disasm::code_printer::{self, CodePrinter};
-use code_printer::CodeWriter as _;
 use itertools::Itertools;
+use colored::*;
 
 use super::{
     instructions::{GenericInstruction, InstructionKind, OperandKind},
     model::{BlockId, ProgramModel},
-    ssa_form::SsaVar,
+    ssa_form::{PhiFunction, SsaBlock, SsaFunction, SsaVar},
 };
 
-pub fn pretty_print_ssa(model: &ProgramModel) -> String {
-    let mut printer = CodePrinter::new();
+fn format_ssa_var(var: &SsaVar) -> String {
+    match var.operand.kind {
+        // Highlight relative memory accesses in cyan
+        super::instructions::OperandKind::RelativeMemory(offset) => {
+            if offset == 0 {
+                "[R]".cyan().to_string()
+            } else if offset > 0 {
+                format!("[R+{}]", offset).cyan().to_string()
+            } else {
+                format!("[R{}]", offset).cyan().to_string()
+            }
+        }
+        // Highlight memory accesses in yellow
+        super::instructions::OperandKind::Memory(addr) => {
+            format!("[{}]", addr).yellow().to_string()
+        }
+        // Highlight immediate values in green
+        super::instructions::OperandKind::Immediate(val) => {
+            format!("{}", val).green().to_string()
+        }
+        // Highlight dereferences in magenta
+        super::instructions::OperandKind::Deref(addr) => {
+            format!("*{}", addr).magenta().to_string()
+        }
+    }
+}
 
-    for (function_id, function) in model
-        .get_ssa_result()
-        .unwrap()
-        .functions
+fn format_phi_function(phi: &PhiFunction) -> String {
+    let inputs = phi
+        .inputs
         .iter()
-        .sorted_by_key(|(id, _)| id.index())
-    {
-        // Print function header
-        printer.line(&format!("Function @{}:", function_id));
+        .map(|(block_id, var)| format!("{}: {}", block_id, format_ssa_var(var)))
+        .join(", ");
+    format!(
+        "{} = φ({})",
+        format_ssa_var(&phi.result),
+        inputs
+    )
+}
 
-        let callee_info = model
-            .get_function_call_analysis()
-            .and_then(|fa| fa.callee_info.get(&function_id));
-        if let Some(callee_info) = callee_info {
-            printer.line(&format!(
-                "parameters: ({}) returns: {}",
-                callee_info.parameter_entry_vars.values().join(", "),
-                callee_info.return_writes.values().join(", ")
-            ));
+fn format_instruction(instr: &GenericInstruction<SsaVar>) -> String {
+    match &instr.kind {
+        InstructionKind::Add(a, b, c) => {
+            format!(
+                "{} = {} + {}",
+                format_ssa_var(c),
+                format_ssa_var(a),
+                format_ssa_var(b)
+            )
         }
-
-        // Sort blocks by address
-        let mut block_ids: Vec<&BlockId> = function.blocks.keys().collect();
-        block_ids.sort_by_key(|id| id.index());
-
-        // Process each block
-        for &block_id in &block_ids {
-            let block = &function.blocks[block_id];
-
-            // Print block header with a separator
-            printer.line(&format!("Block {}: ", block_id.index()));
-
-            // Print phi functions at the beginning of the block
-            let mut indented = printer.indented();
-
-            if !block.phi_functions.is_empty() {
-                indented.line("# Phi functions:");
-                for phi in &block.phi_functions {
-                    let mut sources = Vec::new();
-                    for (&pred_id, var) in &phi.inputs {
-                        sources.push(format!("{}: {}", pred_id, var));
-                    }
-
-                    let sources_str = if sources.is_empty() {
-                        "<empty>".to_string()
-                    } else {
-                        sources.join(", ")
-                    };
-
-                    indented.line(&format!("{} = φ({})", phi.result, sources_str));
-                }
-                indented.line(""); // Extra space after phi functions
-            }
-
-            // Print instructions
-            if !block.instructions.is_empty() {
-                indented.line("# Instructions:");
-                for instr in &block.instructions {
-                    let instruction_str = format!(
-                        "{:<8}  {}",
-                        format!("{}", instr.id.index()),
-                        format!("{}", instr)
-                    );
-                    indented.line(&instruction_str);
-                }
-                indented.line(""); // Extra space after instructions
-            }
-
-            // Blank line between blocks
-            printer.line("");
+        InstructionKind::Mul(a, b, c) => {
+            format!(
+                "{} = {} * {}",
+                format_ssa_var(c),
+                format_ssa_var(a),
+                format_ssa_var(b)
+            )
         }
+        InstructionKind::Input(a) => {
+            format!("{} = input()", format_ssa_var(a))
+        }
+        InstructionKind::Output(a) => {
+            format!("output({})", format_ssa_var(a))
+        }
+        InstructionKind::JumpIfTrue(cond, target) => {
+            format!(
+                "if {} goto {}",
+                format_ssa_var(cond),
+                format_ssa_var(target)
+            )
+        }
+        InstructionKind::JumpIfFalse(cond, target) => {
+            format!(
+                "if !{} goto {}",
+                format_ssa_var(cond),
+                format_ssa_var(target)
+            )
+        }
+        InstructionKind::LessThan(a, b, c) => {
+            format!(
+                "{} = {} < {}",
+                format_ssa_var(c),
+                format_ssa_var(a),
+                format_ssa_var(b)
+            )
+        }
+        InstructionKind::Equals(a, b, c) => {
+            format!(
+                "{} = {} == {}",
+                format_ssa_var(c),
+                format_ssa_var(a),
+                format_ssa_var(b)
+            )
+        }
+        InstructionKind::AdjustRelativeBase(a) => {
+            format!("R += {}", format_ssa_var(a))
+        }
+        InstructionKind::Halt => "halt".red().to_string(),
+        InstructionKind::Data(values) => {
+            format!("DATA {}", values.iter().map(|v| v.to_string().green()).join(", "))
+        }
+        InstructionKind::Goto(target) => {
+            format!("goto {}", format_ssa_var(target))
+        }
+        InstructionKind::Assign(target, source) => {
+            format!(
+                "{} = {}",
+                format_ssa_var(target),
+                format_ssa_var(source)
+            )
+        }
+    }
+}
 
-        // Extra blank line between functions
-        printer.line("");
+fn format_block(block: &SsaBlock) -> String {
+    let mut lines = Vec::new();
+
+    // Block header with line number in gray
+    lines.push(format!(
+        "{}:",
+        format!("{}", block.original_id.index()).blue()
+    ));
+
+    // Phi functions
+    for phi in &block.phi_functions {
+        lines.push(format!("{:<8}{}", "", format_phi_function(phi)));
     }
 
-    printer.result()
+    // Instructions
+    for instr in &block.instructions {
+        lines.push(format!("{:<8}{}", "", format_instruction(instr)));
+    }
+
+    lines.join("\n")
+}
+
+fn format_function(function: &SsaFunction) -> String {
+    let mut blocks: Vec<_> = function.blocks.values().collect();
+    blocks.sort_by_key(|b| b.original_id);
+
+    blocks.iter().map(|b| format_block(b)).join("\n\n")
+}
+
+pub fn pretty_print_ssa(model: &ProgramModel) -> String {
+    let ssa = model.get_ssa_result().expect("No SSA result available");
+
+    let mut functions: Vec<_> = ssa.functions.values().collect();
+    functions.sort_by_key(|f| f.original_id);
+
+    functions
+        .iter()
+        .map(|f| {
+            format!(
+                "function_{} {{\n{}\n}}",
+                f.original_id,
+                format_function(f)
+                    .lines()
+                    .map(|l| format!("    {}", l))
+                    .join("\n")
+            )
+        })
+        .join("\n\n")
 }
