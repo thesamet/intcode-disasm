a56ea3a085d999c1584f159d697c99b7f3215caf
Nadav Samet <thesamet@gmail.com>
2025-05-13 11:52:53 -0700

    Fixed folding test

--- src/disasm/v3/folded_ssa/tests.rs
+++ src/disasm/v3/folded_ssa/tests.rs
@@ -1,31 +1,10 @@
 #[cfg(test)]
 mod tests {
-    
 
     use crate::disasm::{
         test_utils::TestContextBuilder,
-        v3::{model::FoldedSsaComplete, pretty_print::pretty_print_folded_ssa},
+        v3::{common::formatting::ContextualPrettyPrint, model::FoldedSsaComplete},
     };
-
-    // Helper macro for creating versioned memory references, similar to ssa/tests.rs
-    macro_rules! vmr {
-        ($name:ident, $version:expr) => {
-            SsaMemoryReference::versioned(stringify!($name), $version)
-        };
-        ($name:ident) => {
-            SsaMemoryReference::versioned(stringify!($name), 0)
-        };
-    }
-
-    // Helper macro for creating expressions from versioned memory references
-    macro_rules! expr_vmr {
-        ($name:ident, $version:expr) => {
-            Expression::Addressable(vmr!($name, $version))
-        };
-        ($name:ident) => {
-            Expression::Addressable(vmr!($name))
-        };
-    }
 
     #[test]
     fn test_placeholder_folded_ssa() {
@@ -39,7 +18,11 @@ mod tests {
             "#,
         )
         .unwrap();
-        println!("{}", pretty_print_folded_ssa(&ctx.model));
-        assert!(false);
+        let inst = &ctx
+            .main_function()
+            .block(&ctx.main_function().entry_block())
+            .folded_ssa()
+            .instructions[0];
+        assert_eq!(inst.nocolor(), "[R-1]_3 = ([R-2]_0 * 5 + 7) * [R-3]_0");
     }
 }
